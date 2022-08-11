9c600cd31e241985a4ff9019bfde2cc3ee407533
gabrielisilvas <74477557+gabrielisilvas@users.noreply.github.com>
2022-08-11 11:12:34 -0400

    Update chats.php

--- chats.php
+++ chats.php
@@ -1,13 +1,13 @@
 <?php
 set_time_limit(120);
-$chats = [];
-$api_token = "";
-$companieId= "";
+$chats= [];
+$api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjEyZDM2YjQwMTNlMzhhOTNmM2M2OTU1MzFlZWE3Y2RjYmEwN2U3YTBjYWE4MmRjZTIxZjgxYmZhMzgwMGU5MTNjMGU3ZjZiNjk2ZDMwNWNjIn0.eyJhdWQiOiJBUFAtMDAzNjliMTEtZDIzMC00NjY3LWIyYTItNDRjYmU2MzNjNzBjIiwianRpIjoiMTJkMzZiNDAxM2UzOGE5M2YzYzY5NTUzMWVlYTdjZGNiYTA3ZTdhMGNhYTgyZGNlMjFmODFiZmEzODAwZTkxM2MwZTdmNmI2OTZkMzA1Y2MiLCJpYXQiOjE2NjAyMjkzMTgsIm5iZiI6MTY2MDIyOTMxOCwiZXhwIjoxNjc2MTI2OTE4LCJzdWIiOiI4MjY5OCIsInNjb3BlcyI6WyJpbnN0YWxsX2FwcCIsInJlYWRfYWdlbnRfcHJvZmlsZSJdfQ.vc3wIjb1_TqOi8_B2xlrRSnG63GXkd4YzRuKnuFsjgcx_k9D7jPztPHKa3Xy0mWMRCOtg3YQxC5GEi8qhSfi7K8OysZr9xKdMULcqmNVGxGUh9yVlHxFyCv9-Wos3F2dy8wq7Qg9gG8R3Qyb5HLbv5qmv46fWYt-bwUT9ig-J5I";
+$companieId= "11732";
 
 function Chats($chats_id, $api_token, $companieId){
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://huggynew.app/chats/".$chatsId."/chats/".$chats_id);
+curl_setopt($ch, CURLOPT_URL, "https://api.huggy.app/v3/companies/11732/chats".$companieId."/chats/".$chats_id);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
@@ -29,4 +29,3 @@ for($i=0;$i < count($chats); $i++){
   echo "<br/>";
   Chats($chats[$i], $api_token,$companieId);
 }
- 
